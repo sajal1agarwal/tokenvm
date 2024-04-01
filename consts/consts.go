0f4c8428dd73790d7f65f1785a1ad131f505afd0
sajal1agarwal <126244956+sajal1agarwal@users.noreply.github.com>
2024-04-01 23:21:55 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "TOKEN"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "TEVM"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "TKN"
 )
 
 var ID ids.ID
